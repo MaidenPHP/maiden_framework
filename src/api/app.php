5f0c4d9c40f1fa697ecdb94b12a0a7ddcbb0060a
yichen <yichenzhu1337@gmail.com>
2016-09-18 13:09:22 -0400

    added strict_types=1

--- src/api/app.php
+++ src/api/app.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * scalar and return type declaration:
+ * int
+ * float
+ * bool
+ * string
+ * array
+ *
+ * you will get TypeError if conditions are not met properly
+ */
+declare(strict_types=1);
+
 // composer autoload
 require __DIR__ . '/../../vendor/autoload.php';
 
